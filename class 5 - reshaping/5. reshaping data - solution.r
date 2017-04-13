a55e6776864336b4d97a579ba31b7baea031637a
jonathanlittel <rouefully@gmail.com>
2017-04-13 16:13:51 -0400

    add long to wide reshape

--- class 5 - reshaping/5. reshaping data - solution.r
+++ class 5 - reshaping/5. reshaping data - solution.r
@@ -1,42 +1,53 @@
 # load libraries
-  library(dplyr) # for general manipulation
-  library(tidyr) # for reshaping with 'gather'
-  library(xlsx)  # for reading/writing excel files
+library(dplyr) # for general manipulation
+library(tidyr) # for reshaping with 'gather'
+library(xlsx)  # for reading/writing excel files
 
 # first, take a look at this article on reshaping:
-  # https://blog.rstudio.org/2014/07/22/introducing-tidyr/
+# https://blog.rstudio.org/2014/07/22/introducing-tidyr/
 
 # load rev data from social scorecards/alex db (from 1999 - 2015)
-  # pick one of the two methods for reading the data
-  filename1 <- 'client_revenue_alex.csv'  # from a folder
-  filename2 <- 'https://rootcapital.box.com/shared/static/4xi2sgwswfl26nllticutooawq5ho5el.csv' # direct from box
-  rev <- read.csv(filename2, skip=1)   # skip the first row, since we don't need it
+# pick one of the two methods for reading the data
+filename1 <- 'client_revenue_alex.csv'  # from a folder
+filename2 <- 'https://rootcapital.box.com/shared/static/4xi2sgwswfl26nllticutooawq5ho5el.csv' # direct from box
+rev <- read.csv(filename2, skip=1)   # skip the first row, since we don't need it
 
 # format data
-  rev <- rev[-nrow(rev)] # remove the last row 'grand total'
+rev <- rev[-nrow(rev)] # remove the last row 'grand total'
+
+# look at the dota
+head(rev)
 
 # select only the columns Account.Name, and from rev_1996 to rev_2014
-  rev <-  select(rev, Account.Name, rev_1996:rev_2014) 
+rev <-  select(rev, Account.Name, rev_1996:rev_2014) 
 
 # reshape long to wide using 'gather'
-  rev_long <- gather(rev, Year, revenue, rev_1996:rev_2014)
-  
+rev_long <- gather(rev, Year, revenue, rev_1996:rev_2014)
+
 # remove the stub 'rev_' from 'rev_YYYY'  
-  rev_long$Year <- as.numeric(gsub('rev_', '', rev_long$Year))
+rev_long$Year <- as.numeric(gsub('rev_', '', rev_long$Year))
 
 # extra credit:
 # try doing both steps at once, using %>%
 # select and reshape long to wide  
-  rev_long <- rev %>%
-    select(Account.Name, rev_1996:rev_2014) %>%
-    gather(Year, revenue, rev_1996:rev_2014)
- 
+rev_long <- rev %>%
+  select(Account.Name, rev_1996:rev_2014) %>%
+  gather(Year, revenue, rev_1996:rev_2014)
+
+# look at the output
+head(rev_long)
+
 # write output
-  out <- rev_long
-  write.csv(out, 'revenue_long.csv')
-  file.show('revenue_long.csv')
+out <- rev_long
+write.csv(out, 'revenue_long.csv')
+file.show('revenue_long.csv')
+
+
+# go back from long form, to wide form
+rev_wide <- spread(rev_long, Year, revenue)
+head(rev_wide)
 
 # optional: write output to xlsx  
-  # sheetname <- 'revenue'
-  # write.xlsx(out, 'revenue_long.xlsx', sheetName = sheetname) # showNA = FALSE to leave NA values as empty cells
-  # file.show('revenue_long.xlsx')
+# sheetname <- 'revenue'
+# write.xlsx(out, 'revenue_long.xlsx', sheetName = sheetname) # showNA = FALSE to leave NA values as empty cells
+# file.show('revenue_long.xlsx')
